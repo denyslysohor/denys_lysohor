7cec348d123644f1d1139f02f61a4fbf9b82786f
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-05-20 19:38:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace RegistrationForm
 {
@@ -8,30 +9,17 @@ namespace RegistrationForm
     {
         static void Main(string[] args)
         {
-            ExistUser guy = new ExistUser("lmao21", "Ivan", 80);
-            ExistUser guy1 = new ExistUser("lam", "Petro", 80);
-            ExistUser guy2 = new ExistUser("", "Petro", 80);
+            ExistUser guy1 = new ExistUser { Login = "lmao11", Firstname = "Denys", Age = 80 };
+            ExistUser guy3 = new ExistUser { Login = "lmao11", Firstname = "Denys", Age = 80 };
+            ExistUser guy4 = new ExistUser { Login = "lmao112", Firstname = "Denys", Age = 80 };
+            ExistUser guy5 = new ExistUser { Login = "lmao111", Firstname = "Denys", Age = 80 };
+            ExistUser guy2 = new ExistUser { Firstname = "Ivan", Age = 80  };
             UserService userService = new UserService();
-            Validate(guy);
-            userService.Register(guy);
-            Validate(guy1);
             userService.Register(guy1);
-            Validate(guy2);
             userService.Register(guy2);
-        }
-        private static void Validate(User user)
-        {
-            var results = new List<ValidationResult>();
-            var context = new ValidationContext(user);
-            if (!Validator.TryValidateObject(user, context, results, true))
-            {
-                foreach (var error in results)
-                {
-                    Console.WriteLine(error.ErrorMessage);
-                }
-            }
-            else
-                Console.WriteLine("Пользователь прошел валидацию");
+            userService.Register(guy5);
+            userService.Register(guy4);
+            userService.Register(guy3);
         }
     }
 }
