ba27932d6e837cde74e4551f61e6d309073d34ff
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-06-02 20:41:12 +0300

    Homework for 03.06.2021
    This commit consists two files: Observer & Reader/Subscriber patterns.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Supervisor
+{
+    public class Subject
+    {
+        public event EventHandler eventHandler;
+        public void Notify()
+        {
+            if (eventHandler != null)
+            {
+                eventHandler(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public class Observer
+    {
+        Subject subject;
+
+        public Observer(Subject subject)
+        {
+            this.subject = subject;
+        }
+        public void EventTriggering(object sender, EventArgs ev)
+        {
+            Console.WriteLine("Событие затронуто");
+        }
+
+        public void Subscribing()
+        {
+            subject.eventHandler += EventTriggering;
+        }
+
+        public void UnSubscribing()
+        {
+            subject.eventHandler -= EventTriggering;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject sbj = new Subject();
+            Observer observer = new Observer(sbj);
+
+            observer.Subscribing();
+            observer.Subscribing();
+
+            sbj.Notify();
+
+            observer.UnSubscribing();
+
+            sbj.Notify();
+        }
+    }
+}
