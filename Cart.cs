0da4abbd238b99d0eb082f43b44f45f1312c2df2
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-07-01 14:47:49 +0300

    HW for 01.07.2021

--- Cart.cs
+++ Cart.cs
@@ -8,10 +8,10 @@ namespace ShopInfoEF6App
         public int UserId { get; set; }
         public int DiscountId { get; set; }
 
-        public virtual Discount Discount { get; set; }
+        public Discount Discount { get; set; }
 
-        public virtual User User { get; set; }
+        public User User { get; set; }
 
-        public virtual ICollection<CartItem> CartItems { get; set; }
+        public ICollection<CartItem> CartItems { get; set; }
     }
 }
