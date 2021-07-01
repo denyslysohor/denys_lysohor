b9b63cf3d727908485cca7db48e180d695c6e3b3
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-07-01 14:49:10 +0300

    HW for 01.07.2021

--- Product.cs
+++ Product.cs
@@ -9,6 +9,6 @@ namespace ShopInfoEF6App
         public string Name { get; set; }
         public decimal Price { get; set; }
 
-        public virtual ICollection<CartItem> CartItems { get; set; }
+        public ICollection<CartItem> CartItems { get; set; }
     }
 }
