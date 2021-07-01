3125875b8d780ae6d28b6f83398bf08dc92fec6c
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-07-01 14:48:33 +0300

    HW for 01.07.2021

--- Category.cs
+++ Category.cs
@@ -7,6 +7,6 @@ namespace ShopInfoEF6App
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public virtual ICollection<Product> Products { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 }
