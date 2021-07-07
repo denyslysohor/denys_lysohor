6c6d59dd1d0fca06018ed6e434fea37a7b9c9121
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-07-07 11:43:28 +0300

    Bug fixes

---
+++ DataProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaFile
+{
+    class DataProcessor
+    {
+        private List<User> _users;
+        private List<Order> _orders;
+
+        public DataProcessor(List<User> users, List<Order> orders)
+        {
+            _users = users;
+            _orders = new List<Order>();
+        }
+
+        public string GetResults()
+        {
+            var printInfo = from user in _users
+                            join order in _orders
+                            on user.Id equals order.User_id
+                            where user.Age > 18 && user.Age < 65 && order.Order_date > DateTime.Now.AddDays(-7)
+                            orderby order.Order_date ascending
+                            select new { order.Order_number, order.Order_date, user.Name, order.Total };
+            string result = String.Empty;
+            foreach(var temp in printInfo)
+            {
+                result += temp.Order_number + ";";
+                result += temp.Order_date + ";";
+                result += temp.Name + ";";
+                result += temp.Total + ";";
+                result += "\n";
+            } 
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaFile
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<User> users = await FileProcessor.GetUsersAsync();
+            List<Order> orders = await FileProcessor.GetOrdersAsync();
+
+            DataProcessor dP = new DataProcessor(users, orders);
+
+           foreach(User user in users)
+            {
+                Console.WriteLine(user.Id+"|"+user.Name + "|" +user.Gender + "|" +user.Age);
+            }
+            Console.WriteLine();
+
+            foreach (Order order in orders)
+            {
+                Console.WriteLine(order.Id + "|" +order.User_id + "|" +order.Order_number + "|" + order.Order_date + "|" + order.Total);
+            }
+            Console.WriteLine();
+            Console.WriteLine(dP.GetResults());
+
+            FileProcessor.SaveFile(dP.GetResults());
+
+            Console.ReadKey();
+        }
+    }
+}
