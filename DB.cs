cc379717f55d4cfd865663f008895e2b8844f050
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-07-23 15:30:53 +0300

    UPD HW

---
+++ DB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaptopApi.Models
+{
+    public class DB
+    {
+        public List<Laptop> Laptops { get; set; }
+        public List<Shop> Shops { get; set; }
+
+        public DB(List<Shop> shops, List<Laptop> laptops)
+        {
+            shops = Shops;
+            laptops = Laptops;
+        }
+    }
+}
+
