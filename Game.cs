d91ac3a71550e3752ba32dbeb2a11aaee0f86f7d
denyslysohor <78100050+denyslysohor@users.noreply.github.com>
2021-09-02 17:47:43 +0300

    Updated files

---
+++ Game.cs
@@ -0,0 +1,18 @@
+﻿namespace RockPaperScissors.Data.Model
+{
+    using System;
+    using RockPaperScissors.Data.Model.Enums;
+
+    public class Game
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Player FirstPlayer { get; set; }
+
+        public Player SecondPlayer { get; set; }
+
+        public GameStatus Status { get; set; }
+    }
+}
